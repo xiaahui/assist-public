84ecb8cb0cec8d41575ae3a2802c14d89e4b51d7
Robert Hilbrich <robert.hilbrich@fokus.fraunhofer.de>
2014-07-02 08:57:56 +0200

    Fixed ShowCheatSheet Menu entry

--- ch.hilbri.assist.application/src/ch/hilbri/assist/application/handlers/ShowUsageCheatSheetHandler.java
+++ ch.hilbri.assist.application/src/ch/hilbri/assist/application/handlers/ShowUsageCheatSheetHandler.java
@@ -3,8 +3,7 @@ package ch.hilbri.assist.application.handlers;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.cheatsheets.OpenCheatSheetAction;
 
 import ch.hilbri.assist.application.helpers.PathProvider;
 
@@ -15,10 +14,6 @@ public class ShowUsageCheatSheetHandler {
 	
 	@Execute
 	public void execute(IEclipseContext context, IWorkbench workbench) {
-		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(PathProvider.ECL_CHEAT_SHEET_VIEW);
-		} catch (PartInitException e) {
-			e.printStackTrace();
-		}
+			new OpenCheatSheetAction(PathProvider.APP_CHEAT_SHEET_ID).run();
 	}
 }
