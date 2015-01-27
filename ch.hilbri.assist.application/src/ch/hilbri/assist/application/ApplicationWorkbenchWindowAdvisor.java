0aef879bf59be2d6ccb235f1e84ed80ed7a6b69e
Robert Hilbrich <Robert@Hilbri.ch>
2015-01-27 16:14:57 +0100

    Do not show cheat sheet after startup

--- ch.hilbri.assist.application/src/ch/hilbri/assist/application/ApplicationWorkbenchWindowAdvisor.java
+++ ch.hilbri.assist.application/src/ch/hilbri/assist/application/ApplicationWorkbenchWindowAdvisor.java
@@ -7,10 +7,8 @@ import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
-import org.eclipse.ui.cheatsheets.OpenCheatSheetAction;
 
 import ch.hilbri.assist.application.helpers.Helpers;
-import ch.hilbri.assist.application.helpers.PathProvider;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -31,7 +29,8 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	}
 
 	public void postWindowCreate() {
-		new OpenCheatSheetAction(PathProvider.APP_CHEAT_SHEET_ID).run();
+//		Do not show the cheat sheet automatically after start up
+//		new OpenCheatSheetAction(PathProvider.APP_CHEAT_SHEET_ID).run();
 	}
 
 	@Override
