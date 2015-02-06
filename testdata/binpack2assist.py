632e92860986913257523b0d978fe0cef4d77cbd
Michael Behrisch <behrisch@users.sourceforge.net>
2015-02-07 00:42:17 +0100

    parsing bppc, allow for scaling

--- testdata/binpack2assist.py
+++ testdata/binpack2assist.py
@@ -3,15 +3,68 @@
 # to assist instances
 
 from __future__ import print_function
-import sys
+import sys, argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('files', metavar='<file>', nargs='+',
+                    help='files to parse')
+parser.add_argument("-n", "--number", type=int, help="set fixed number of bins/boards")
+parser.add_argument("-s", "--scale", type=float, default=1.0, help="scale number of bins/boards")
+parser.add_argument("-c", "--capacity-scale", type=float, default=1.0, help="scale capacity of bins/boards")
+args = parser.parse_args()
 
-for f in sys.argv[1:]:
+def readBPPC(numItems, cap, r):
+    intCap = int(cap * args.capacity_scale)
+    numBoards = args.number if args.number else int(numBins * args.scale)
+    items = []
+    with open(r.name[:-4] + ".mdsl", 'w') as w:
+        print("""\
+Global {
+    System name = "Binpack with conflicts System %s";
+}
+
+Hardware {""" % r.name, file=w)
+
+        for i in range(numBoards):
+            print("""\
+    Board Board%s {
+        Processor Processor%s {
+            Core Core%s {
+                Capacity = %s;
+            }
+        }
+    }""" % (i, i, i, intCap), file=w)
+
+        print("}\n\nSoftware {", file=w)
+
+        for i in range(int(numItems)):
+            items.append(r.readline().split())
+            print("""\
+    Application A%s {
+        Core-utilization = %s;
+    }""" % (items[-1][0], int(float(items[-1][1]))), file=w)
+
+        print("}\n\nRelations {", file=w)
+        for item in items:
+            for conflict in item[2:]:
+                print("    A%s,A%s dislocal up to Board;" % (item[0], conflict), file=w)
+
+        print("}\n\n", file=w)
+
+
+
+for f in args.files:
     with open(f) as r:
-        numProbs = int(r.readline())
+        first = r.readline().split()
+        if len(first) == 2:
+            readBPPC(int(first[0]), float(first[1]), r)
+            continue
+        numProbs = int(first[0])
         print("Converting %s problems from %s" % (numProbs, f))
         for p in range(numProbs):
             desc = r.readline().strip()
-            cap, numItems, numBins = [int(float(x)) for x in r.readline().split()]
+            cap, numItems, numBins = [float(x) for x in r.readline().split()]
+            intCap = int(cap * args.capacity_scale)
+            numBoards = args.number if args.number else int(numBins * args.scale)
             with open(desc + ".mdsl", 'w') as w:
                 print("""\
 Global {
@@ -20,7 +73,7 @@ Global {
 
 Hardware {""" % desc, file=w)
 
-                for i in range(numBins):
+                for i in range(numBoards):
                     print("""\
         Board Board%s {
                 Processor Processor%s {
@@ -28,11 +81,11 @@ Hardware {""" % desc, file=w)
                                 Capacity = %s;
                         }
                 }
-        }""" % (i, i, i, cap), file=w)
+        }""" % (i, i, i, intCap), file=w)
 
                 print("}\n\nSoftware {", file=w)
 
-                for i in range(numItems):
+                for i in range(int(numItems)):
                     print("""\
         Application A%s {
                 Core-utilization = %s;
