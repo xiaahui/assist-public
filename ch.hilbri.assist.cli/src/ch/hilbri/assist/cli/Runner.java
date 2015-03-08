0a753777bc307272cd1c1eb1b15e3beae2751a4c
Michael Behrisch <behrisch@users.sourceforge.net>
2015-03-08 18:50:52 +0100

    adding cli and test data

---
+++ ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
@@ -0,0 +1,66 @@
+package ch.hilbri.assist.cli;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+
+import ch.hilbri.assist.datamodel.model.AssistModel;
+import ch.hilbri.assist.datamodel.model.Connector;
+import ch.hilbri.assist.datamodel.model.EqInterface;
+import ch.hilbri.assist.datamodel.model.ModelPackage;
+import ch.hilbri.assist.datamodel.result.mapping.Result;
+import ch.hilbri.assist.mapping.solver.AssistSolver;
+import ch.hilbri.assist.mapping.solver.SearchType;
+import ch.hilbri.assist.mapping.solver.exceptions.BasicConstraintsException;
+import ch.hilbri.assist.mappingdsl.MappingDSLInjectorProvider;
+
+import com.google.inject.Inject;
+
+public class Runner {
+
+	@Inject
+	protected ParseHelper<AssistModel> parser;
+
+	public static void main(String[] args) throws Exception {
+		final Options options = new Options();
+		options.addOption("s", "solutions", true, "the number of solution to find");
+		final CommandLineParser parser = new BasicParser();
+		final CommandLine cmd = parser.parse(options, args);
+		final int numSolutions = Integer.parseInt(cmd.getOptionValue("solutions", "1"));
+		
+	    ModelPackage.eINSTANCE.eClass();
+		final Runner runner = new Runner();
+		new MappingDSLInjectorProvider().getInjector().injectMembers(runner);
+
+		for (String arg: cmd.getArgs()) {
+			final AssistModel model = runner.parser.parse(IOUtils.toString(new FileReader(arg)));
+			final AssistSolver solver = new AssistSolver(model);
+			solver.setSolverSearchStrategy(SearchType.CONSECUTIVE);
+			solver.setSolverMaxSolutions(numSolutions);
+			try {
+				solver.propagation();
+				solver.solutionSearch();
+				final ArrayList<Result> results = solver.getResults();
+				System.out.println(results.size() + " solutions found.");
+				for (Result r: results) {
+					for (Entry<EqInterface, Connector> e: r.getMapping().entrySet()) {
+						System.out.println(e.getKey().getName() + " -> " + e.getValue().getName());
+					}
+					System.out.println();
+				}
+			}
+			catch (BasicConstraintsException e) {
+				System.err.println("Inconsistency found while processing constraint \"" + e.getConstraintName() + "\"");
+				System.err.println("\""+ e.getExplanation() + "\"");
+			}
+		}
+	}
+
+}
---
+++ testdata/binpack2assist.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# This script converts bin packing instances from http://people.brunel.ac.uk/~mastjjb/jeb/orlib/binpackinfo.html
+# to assist instances
+
+from __future__ import print_function
+import os, sys, argparse, zipfile, subprocess, time
+
+def readBPPC(numItems, cap, input, bound):
+    base = os.path.basename(input.name)
+    intCap = int(cap * args.capacity_scale)
+    numBoards = args.number if args.number else int(bound[base] * args.bin_scale)
+    items = []
+    with open(base[:-4] + ".mdsl", 'w') as w:
+        print("""\
+Global {
+    System name = "Binpack with conflicts System %s";
+}
+
+Hardware {""" % base, file=w)
+
+        for i in range(numBoards):
+            print("""\
+    Board Board%s {
+        Processor Processor%s {
+            Core Core%s {
+                Capacity = %s;
+            }
+        }
+    }""" % (i, i, i, intCap), file=w)
+
+        print("}\n\nSoftware {", file=w)
+
+        for i in range(int(numItems)):
+            items.append(input.readline().split())
+            print("""\
+    Application A%s {
+        Core-utilization = %s;
+    }""" % (items[-1][0], int(float(items[-1][1]))), file=w)
+
+        print("}\n\nRelations {", file=w)
+        for item in items:
+            for conflict in item[2:]:
+                print("    A%s,A%s dislocal up to Board;" % (item[0], conflict), file=w)
+
+        print("}\n\n", file=w)
+    return w.name
+
+def readFile(input):
+    first = input.readline().split()
+    try:
+        f0 = int(first[0])
+    except:
+        return None
+    if len(first) == 2:
+        return readBPPC(f0, float(first[1]), input, bound)
+    print("Converting %s problems from %s" % (f0, f))
+    for p in range(f0):
+        desc = input.readline().strip()
+        cap, numItems, numBins = [float(x) for x in input.readline().split()]
+        intCap = int(cap * args.capacity_scale)
+        numBoards = args.number if args.number else int(numBins * args.bin_scale)
+        with open(desc + ".mdsl", 'w') as w:
+            print("""\
+Global {
+        System name = "Binpack System %s";
+}
+
+Hardware {""" % desc, file=w)
+
+            for i in range(numBoards):
+                print("""\
+        Board Board%s {
+                Processor Processor%s {
+                        Core Core%s {
+                                Capacity = %s;
+                        }
+                }
+        }""" % (i, i, i, intCap), file=w)
+
+            print("}\n\nSoftware {", file=w)
+
+            for i in range(int(numItems)):
+                print("""\
+        Application A%s {
+                Core-utilization = %s;
+        }""" % (i, int(float(input.readline()))), file=w)
+
+            print("}\n", file=w)
+    return w.name
+
+def runAssist(inputs, args):
+    procs = []
+    for i in inputs:
+        if not i:
+            continue
+        procs.append(subprocess.Popen(["java", "-jar", args.jar, i, "-s", str(args.solutions)],
+                                      stdout=open(i+".log", 'w'), stderr=subprocess.STDOUT))
+        if len(procs) == args.instances:
+            if args.timeout == 0:
+                for p in procs:
+                    p.wait()
+            else:
+                time.sleep(args.timeout)
+                for p in procs:
+                    p.kill()
+            procs = []
+
+def getEntry(line):
+    return line.split(">")[1].split("<")[0]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='<file>', nargs='+',
+                        help='files to parse')
+    parser.add_argument("-n", "--number", type=int, help="set fixed number of bins/boards")
+    parser.add_argument("-b", "--bin-scale", type=float, default=1.0, help="scale number of bins/boards")
+    parser.add_argument("-c", "--capacity-scale", type=float, default=1.0, help="scale capacity of bins/boards")
+    parser.add_argument("-r", "--bppc-results", help="results file for BPPC to read the bin number from")
+    parser.add_argument("-j", "--jar", default=os.path.join(os.path.dirname(__file__), '..', 'ch.hilbri.assist.cli', 'assist_cli.jar'), help="jar file to start")
+    parser.add_argument("-i", "--instances", type=int, default=0, help="number of parallel instances to start")
+    parser.add_argument("-t", "--timeout", type=int, default=0, help="timeout in seconds")
+    parser.add_argument("-s", "--solutions", type=int, default=1, help="number of solutions to find")
+    args = parser.parse_args()
+
+    bound = {}
+    if args.bppc_results:
+        with open(args.bppc_results) as results:
+            count = -1
+            for line in results:
+                if "BPPC_" in line and ".txt" in line:
+                    name = getEntry(line)
+                    count = 0
+                if count >= 0:
+                    count += 1
+                if count == 3:
+                    bound[name] = int(getEntry(line))
+                    count = -1
+    mdsls = []
+    for f in args.files:
+        if zipfile.is_zipfile(f):
+            zipf = zipfile.ZipFile(f)
+            for z in zipf.namelist():
+                mdsls.append(readFile(zipf.open(z)))
+        else:
+            mdsls.append(readFile(open(f)))
+
+    if args.instances > 0:
+        runAssist(mdsls, args)
