520c3ac045cc71bd0cea328c3d6cb4530fd5c4f6
Michael Behrisch <oss@behrisch.de>
2015-03-10 16:24:12 +0100

    adding syntax checking to main tree

--- ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
+++ ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
@@ -8,6 +8,10 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.io.IOUtils;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.xtext.junit4.util.ParseHelper;
 
 import ch.hilbri.assist.datamodel.model.AssistModel;
@@ -38,7 +42,29 @@ public class Runner {
 		new MappingDSLInjectorProvider().getInjector().injectMembers(runner);
 
 		for (String arg: cmd.getArgs()) {
-			final AssistModel model = runner.parser.parse(IOUtils.toString(new FileReader(arg)));
+			URI uri = URI.createFileURI(arg);
+			ResourceSet rs = new ResourceSetImpl();
+			Resource resource = rs.getResource(uri, true);
+			
+			/* Searching for errors inside the document? */
+			/* 1) Error with the syntax of the dsl */
+			if (!resource.getErrors().isEmpty()) {
+				System.err.println("Could not parse " + arg + ".");
+				for (Resource.Diagnostic error:  resource.getErrors()) {
+					System.err.println(error.getMessage());
+				}
+				continue;
+			}
+			if (resource.getContents().size() == 0) { 
+				System.err.println(arg + " has no usable data.");
+				continue;
+			}
+			final AssistModel model = (AssistModel) resource.getContents().get(0);
+			/* 2) Custom validation rule errors // enabling those leads to an invalid jar, for a possible workaround see http://zarnekow.blogspot.de/2010/06/how-to-deploy-xtext-standalone.html?showComment=1279241626077#c7153662425903347274
+			if (Diagnostician.INSTANCE.validate(model).getSeverity() == Diagnostic.ERROR) { 
+				System.err.println("Errors on validating " + arg + ".");
+				continue;
+			}*/
 			PostProcessor.createMissingThreads(model);
 			final AssistSolver solver = new AssistSolver(model);
 			solver.setSolverSearchStrategy(SearchType.CONSECUTIVE);
