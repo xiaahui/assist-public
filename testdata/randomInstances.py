d084fca1686b9ec3850b4aa7db43311a1cd75ca1
Michael Behrisch <behrisch@users.sourceforge.net>
2015-03-12 00:28:51 +0100

    including colocality and group dislocality in random instances

--- testdata/randomInstances.py
+++ testdata/randomInstances.py
@@ -3,7 +3,7 @@
 # to assist instances
 
 from __future__ import print_function
-import os, sys, argparse, random
+import os, sys, argparse, random, collections
 
 def parseAbbrevList(s, n):
     l = [int(e) for e in s.split(",")]
@@ -23,8 +23,11 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--interfaces-per-connector", type=int, default=16, help="set fixed number of interfaces per connector")
     parser.add_argument("-n", "--interfaces", type=float, default=0.9, help="set either used fraction (value < 1) or total number of interfaces needed")
     parser.add_argument("-d", "--dislocalities", default="0,10,10", help="set number of dislocalities per level")
+    parser.add_argument("-g", "--dislocality-groups", default="0,10,10", help="set number of dislocality groups per level")
     parser.add_argument("-l", "--colocalities", default="0,10,10", help="set number of colocalities per level")
     parser.add_argument("-p", "--max-interfaces-per-dislocality", default="6", help="set maximum number of interfaces per dislocality on level")
+    parser.add_argument("-m", "--max-sets-per-dislocality-group", default="4", help="set maximum number of sets per dislocality group on level")
+    parser.add_argument("-k", "--max-interfaces-per-dislocality-group-set", default="6", help="set maximum number of interfaces per dislocality group set on level")
     parser.add_argument("-q", "--max-interfaces-per-colocality", default="6", help="set maximum number of interfaces per colocality on level")
     parser.add_argument("-o", "--output", default="random.mdsl", help="name of the output file")
     args = parser.parse_args()
@@ -55,13 +58,13 @@ if __name__ == "__main__":
                         t = random.choice(types)
                         types.remove(t)
                         if s > 0:
-                            print((12*" ") + '"T%s" = %s;' % (t, s), file=w)
+                            print((12*" ") + '"T%02i" = %s;' % (t, s), file=w)
                             for n in range(s):
                                 availInterfaces.append([i,j,k,t])
                     print("        }", file=w)
                 print("    }", file=w)
             print("}", file=w)
-        
+
         if args.interfaces >= 1:
             numUsedInterfaces = int(args.interfaces)
         else:
@@ -69,34 +72,80 @@ if __name__ == "__main__":
         if numUsedInterfaces > len(availInterfaces):
             print("Too many interfaces needed, please adapt --interfaces", file=sys.stderr)
         usedInterfaces = []
+        locations = [collections.defaultdict(list), collections.defaultdict(list), collections.defaultdict(list)]
         while len(usedInterfaces) < numUsedInterfaces:
             iface = random.choice(availInterfaces)
-            usedInterfaces.append(iface)
+            iname = "I%04i" % len(usedInterfaces)
+            usedInterfaces.append((iface, iname))
+            for i in range(3):
+                locations[i][tuple(iface[:i+1])].append(iname)
             availInterfaces.remove(iface)
         print("\nInterfaces {", file=w)
-        for idx, iface in enumerate(usedInterfaces):
+        for iface, name in usedInterfaces:
             print("""\
-    Interface I%s {
-        Type = "T%s";
-        Route = "R%s";
-    }""" % (idx, iface[3], routes[(iface[0], iface[1], iface[2])]), file=w)
+    Interface %s {
+        Type = "T%02i";
+        Route = "R%02i";
+    }""" % (name, iface[3], routes[(iface[0], iface[1], iface[2])]), file=w)
+
+        print("}\n\nInterfaceGroups {", file=w)
+        numGroupDisloc = parseAbbrevList(args.dislocality_groups, 3)
+        maxSetDislocGroup = parseAbbrevList(args.max_sets_per_dislocality_group, 3)
+        maxIFaceDislocSet = parseAbbrevList(args.max_interfaces_per_dislocality_group_set, 3)
+        groupDislocs = []
+        for level, (name, count) in enumerate(zip(["Connector", "RDC", "Compartment"], numGroupDisloc)):
+            for j in range(count):
+                if len(locations[level]) <= 1:
+                    print("Warning! Not enough locations for %s group dislocality." % name)
+                    break
+                numSets = random.randint(2, maxSetDislocGroup[level])
+                sets = [[] for x in xrange(numSets)]
+                setSizes = [random.randint(1, maxIFaceDislocSet[level]) for x in xrange(numSets)]
+                for ifaces in locations[level].values():
+                    random.choice(sets).extend(ifaces)
+                groups = []
+                for idx, (s, size) in enumerate(zip(sets, setSizes)):
+                    while len(s) > size:
+                        s.pop(random.randrange(len(s)))
+                    if len(s) == 0:
+                        print("Warning! Skipping empty set on group dislocality.")
+                    else:
+                        gname = "G%s_%s_%s" % (level, j, idx)
+                        print("    Group %s { %s };" % (gname, ",".join(s)), file=w)
+                        groups.append(gname)
+                if len(groups) <= 1:
+                    print("Warning! Skipping underspecified group dislocality.")
+                else:
+                    groupDislocs.append((groups, name))
 
         print("}\n\nRestrictions {", file=w)
+        for groups, name in groupDislocs:
+            print("    %s dislocal up to %s;" % (",".join(groups), name), file=w)
         numDisloc = parseAbbrevList(args.dislocalities, 3)
         maxIFaceDisloc = parseAbbrevList(args.max_interfaces_per_dislocality, 3)
         for level, (name, count) in enumerate(zip(["Connector", "RDC", "Compartment"], numDisloc)):
             for j in range(count):
-                ifaceLocs = set()
+                locs = list(locations[level].keys())
+                if len(locs) <= 1:
+                    print("Warning! Not enough locations for %s dislocality." % name)
+                    break
+                numIFaces = min(len(locs), random.randint(2, maxIFaceDisloc[level]))
                 ifaces = []
-                for i in range(maxIFaceDisloc[level]):
-                    ifaceIdx = random.randint(0, len(usedInterfaces) - 1)
-                    loc = tuple(usedInterfaces[ifaceIdx][:level+1])
-                    if loc not in ifaceLocs:
-                        ifaces.append("I%s" % ifaceIdx)
-                        ifaceLocs.add(loc)
-                if len(ifaces) > 1:
-                    print("    %s dislocal up to %s;" % (",".join(ifaces), name), file=w)
-        #for i in range(int(numItems)):
-            #print("    G%s on same Connector;" % i, file=w)
+                for i in range(numIFaces):
+                    loc = locs.pop(random.randrange(len(locs)))
+                    ifaces.append(random.choice(locations[level][loc]))
+                print("    %s dislocal up to %s;" % (",".join(ifaces), name), file=w)
+
+        numColoc = parseAbbrevList(args.colocalities, 3)
+        maxIFaceColoc = parseAbbrevList(args.max_interfaces_per_colocality, 3)
+        for level, (name, count) in enumerate(zip(["Connector", "RDC", "Compartment"], numColoc)):
+            for j in range(count):
+                numIFaces = random.randint(2, maxIFaceColoc[level])
+                ifaces = []
+                while len(ifaces) < numIFaces:
+                    ifaces = list(random.choice(locations[level].values()))
+                while len(ifaces) > numIFaces:
+                    ifaces.pop(random.randrange(len(ifaces)))
+                print("    %s on same %s;" % (",".join(ifaces), name), file=w)
 
         print("}\n\n", file=w)
