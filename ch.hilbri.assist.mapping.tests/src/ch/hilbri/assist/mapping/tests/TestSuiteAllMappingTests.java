b644b52c206b456b63ada47677883a66d4350e7c
behr_mi <behr_mi@TS-070016ND.intra.dlr.de>
2015-03-17 10:20:45 +0100

    cleaning tests

--- ch.hilbri.assist.mapping.tests/src/ch/hilbri/assist/mapping/tests/TestSuiteAllMappingTests.java
+++ ch.hilbri.assist.mapping.tests/src/ch/hilbri/assist/mapping/tests/TestSuiteAllMappingTests.java
@@ -4,31 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import ch.hilbri.assist.mapping.tests.constraints.AllApplicationThreadsOnSameBoardTests;
-import ch.hilbri.assist.mapping.tests.constraints.CoreCapacityTests;
-import ch.hilbri.assist.mapping.tests.constraints.DesignAssuranceLevelComplexTests;
-import ch.hilbri.assist.mapping.tests.constraints.DesignAssuranceLevelTests;
-import ch.hilbri.assist.mapping.tests.constraints.DesignAssuranceLevelTestsInvalid;
-import ch.hilbri.assist.mapping.tests.constraints.NoPermutationsAdvancedTests;
-import ch.hilbri.assist.mapping.tests.constraints.NoPermutationsTests;
-import ch.hilbri.assist.mapping.tests.constraints.RAMCapacityTests;
-import ch.hilbri.assist.mapping.tests.constraints.ROMCapacityTests;
+import ch.hilbri.assist.mapping.tests.constraints.DislocalityTests;
 import ch.hilbri.assist.mapping.tests.results.BasicResultTests;
-import ch.hilbri.assist.mapping.tests.results.MultipleResultsTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	BasicResultTests.class,
-	MultipleResultsTests.class,
-	CoreCapacityTests.class,
-	RAMCapacityTests.class,
-	ROMCapacityTests.class,
-	NoPermutationsTests.class,
-	NoPermutationsAdvancedTests.class,
-	AllApplicationThreadsOnSameBoardTests.class,
-	DesignAssuranceLevelTestsInvalid.class,
-	DesignAssuranceLevelTests.class,
-	DesignAssuranceLevelComplexTests.class
+	DislocalityTests.class
 })  
 
 public class TestSuiteAllMappingTests {}
