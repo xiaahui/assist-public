18053ae1d80b552ca74da0dfc1f308f0e6b66927
behr_mi <behr_mi@TS-070016ND.intra.dlr.de>
2015-04-09 12:52:36 +0200

    hardest colocality stub

--- ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
+++ ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
@@ -93,6 +93,7 @@ public class Runner {
 				case "ff": heuristic = SearchType.MIN_DOMAIN_FIRST; break;
 				case "ffmd": heuristic = SearchType.MAX_DEGREE_FIRST; break;
 				case "hd": heuristic = SearchType.HARDEST_DISLOCALITIES_FIRST; break;
+				case "hc": heuristic = SearchType.HARDEST_COLOCALITIES_FIRST; break;
 				case "st": heuristic = SearchType.SCARCEST_IOTYPE_FIRST; break;
 				case "md": heuristic = SearchType.VARS_IN_MOST_DISLOC; break;
 				case "domwd": heuristic = SearchType.DOM_OVER_WDEG; break;
