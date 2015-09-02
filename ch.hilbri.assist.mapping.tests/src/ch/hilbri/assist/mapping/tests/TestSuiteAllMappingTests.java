c25865e928529cd0e622be75b9c607229690e519
Robert Hilbrich <robert@hilbri.ch>
2015-09-02 20:33:07 +0200

    Added the xtext-feature dependency to surefire plugin - no warning of log4j anymore, now all (basic) tests run fine

--- ch.hilbri.assist.mapping.tests/src/ch/hilbri/assist/mapping/tests/TestSuiteAllMappingTests.java
+++ ch.hilbri.assist.mapping.tests/src/ch/hilbri/assist/mapping/tests/TestSuiteAllMappingTests.java
@@ -6,12 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import ch.hilbri.assist.mapping.tests.misc.ChocoSolverTests;
 import ch.hilbri.assist.mapping.tests.misc.SearchTypeTests;
+import ch.hilbri.assist.mapping.tests.strategies.BasicStrategyTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	SearchTypeTests.class,
-	ChocoSolverTests.class
-	
+	ChocoSolverTests.class,
+	BasicStrategyTest.class
 })  
 
 public class TestSuiteAllMappingTests {}
