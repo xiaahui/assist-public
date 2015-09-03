305ff057d075602d0577375ff0a04594301fed39
Robert Hilbrich <robert@hilbri.ch>
2015-09-03 21:12:20 +0200

    Removed the xtend-gen folder from repository

--- ch.hilbri.assist.mapping/xtend-gen/ch/hilbri/assist/mapping/solver/constraints/choco/ACF.java
+++
@@ -1,7 +0,0 @@
-package ch.hilbri.assist.mapping.solver.constraints.choco;
-
-import ch.hilbri.assist.mapping.solver.constraints.choco.AssistConstraintFactory;
-
-@SuppressWarnings("all")
-public final class ACF extends AssistConstraintFactory {
-}
