44c48803dee5966113e5607c146135577f19c4ce
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2017-04-19 08:48:24 +0200

    Added the ability to create an ASSIST project with all Classpath

---
+++ ch.hilbri.assist.gui/src/ch/hilbri/assist/gui/classpath/ASSISTClasspathContainerInitializer.java
@@ -0,0 +1,21 @@
+package ch.hilbri.assist.gui.classpath;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.ClasspathContainerInitializer;
+import org.eclipse.jdt.core.IClasspathContainer;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+
+public class ASSISTClasspathContainerInitializer extends ClasspathContainerInitializer {
+
+
+	@Override
+	public void initialize(IPath containerPath, IJavaProject project) throws CoreException {
+		
+		ASSISTClasspathContainer c = new ASSISTClasspathContainer();
+		JavaCore.setClasspathContainer(containerPath, new IJavaProject[] { project }, new IClasspathContainer[] { c }, null );
+		
+	}
+
+}
