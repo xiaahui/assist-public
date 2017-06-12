75fa3643bf54a9e39e9461aad0bdcc4e322579d9
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2017-06-12 20:00:20 +0200

    Added the ability to have proper toolbars in gui and mapping

---
+++ ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/handlers/OpenNewMappingSpecWizard.java
@@ -0,0 +1,35 @@
+package ch.hilbri.assist.mapping.ui.handlers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.ui.IWorkbenchCommandConstants;
+
+@SuppressWarnings("restriction")
+public class OpenNewMappingSpecWizard {
+	@CanExecute
+	public boolean canExecute(MApplication application, EModelService service) {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject [] projects = workspace.getRoot().getProjects();
+		return projects.length > 0;
+	}
+	
+	@Execute
+	public void execute(EHandlerService hService, ECommandService cService) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(IWorkbenchCommandConstants.FILE_NEW_PARM_WIZARDID, "ch.hilbri.assist.mapping.wizards.newMappingSpecification");
+		ParameterizedCommand cmd = cService.createCommand(IWorkbenchCommandConstants.FILE_NEW, map);
+		if (hService.canExecute(cmd)) 
+				hService.executeHandler(cmd);
+	}
+}
