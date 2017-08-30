f76e099936e44256fc5c45d4b0c25ad2f0a7d9f2
Robert Hilbrich <Robert Hilbrich@iMac.fritz.box>
2017-08-30 08:55:03 +0200

    Move to Oxygen / Xtext 2.12

--- ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
+++ ch.hilbri.assist.cli/src/ch/hilbri/assist/cli/Runner.java
@@ -11,7 +11,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.testing.util.ParseHelper;
 
 import com.google.inject.Inject;
 
