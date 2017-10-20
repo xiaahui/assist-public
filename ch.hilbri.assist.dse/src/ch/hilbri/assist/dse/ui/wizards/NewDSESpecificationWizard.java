44f6aead0fdc5b2c7247a396b0598e4177513d8a
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2017-10-20 11:28:24 +0200

    [#36] Create editor components and wizards for DSE DSL
    Added the basics for the new dse specification editor
    
    Url: http://github.com/roberthilbrich/assist-public/issues/36

---
+++ ch.hilbri.assist.dse/src/ch/hilbri/assist/dse/ui/wizards/NewDSESpecificationWizard.java
@@ -0,0 +1,14 @@
+package ch.hilbri.assist.dse.ui.wizards;
+
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+public class NewDSESpecificationWizard extends BasicNewResourceWizard implements INewWizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
