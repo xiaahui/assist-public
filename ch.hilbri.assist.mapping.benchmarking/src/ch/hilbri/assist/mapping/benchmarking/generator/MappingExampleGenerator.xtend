d56c81adb7a5bc26a136ff8cf198ca482007d4a8
Robert Hilbrich <robert.hilbrich@dlr.de>
2017-12-06 10:43:21 +0100

    [#43] Generate a synthetic mapping example generator
    Added file stubs
    
    Url: https://github.com/roberthilbrich/assist-public/issues/43

---
+++ ch.hilbri.assist.mapping.benchmarking/src/ch/hilbri/assist/mapping/benchmarking/generator/MappingExampleGenerator.xtend
@@ -0,0 +1,9 @@
+package ch.hilbri.assist.mapping.benchmarking.generator
+
+import ch.hilbri.assist.mapping.model.AssistModel
+
+class MappingExampleGenerator {
+	static def AssistModel generateMappingExample() {
+		return null
+	}
+}
---
+++ ch.hilbri.assist.mapping.benchmarking/xtend-gen/ch/hilbri/assist/mapping/benchmarking/generator/MappingExampleGenerator.java
@@ -0,0 +1,10 @@
+package ch.hilbri.assist.mapping.benchmarking.generator;
+
+import ch.hilbri.assist.mapping.model.AssistModel;
+
+@SuppressWarnings("all")
+public class MappingExampleGenerator {
+  public static AssistModel generateMappingExample() {
+    return null;
+  }
+}
