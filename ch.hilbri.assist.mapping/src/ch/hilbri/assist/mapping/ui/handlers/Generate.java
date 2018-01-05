056d63c77d41e5e9c36f8abd1eb3de7dca03e1d9
Robert Hilbrich <Robert.Hilbrich@dlr.de>
2018-01-05 09:25:59 +0100

    Mapping should only be allowed, if we actually look at mdsl file

--- ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/handlers/Generate.java
+++ ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/handlers/Generate.java
@@ -27,29 +27,42 @@ public class Generate {
 
 	@CanExecute
 	public boolean canExecute(MApplication application, EModelService service) {
-		
+
+		/*
+		 * Mapping generation should only be allowed, if we are actually looking at a
+		 * mapping problem
+		 */
+		if (!EditorUtils.getActiveXtextEditor().getLanguageName().equals("ch.hilbri.assist.mapping.dsl.MappingDSL"))
+			return false;
+
+		/*
+		 * If it is a mapping problem, then we should check, whether the content is ok
+		 */
 		IXtextDocument xtextDocument = EditorUtils.getActiveXtextEditor().getDocument();
-		boolean result = xtextDocument.readOnly(
-				new IUnitOfWork<Boolean, XtextResource>() {
-					public Boolean exec(XtextResource resource) throws Exception {
-						EcoreUtil.resolveAll(resource);
-					
-						/* Searching for errors inside the document? */
-						/* - Error with the syntax of the dsl */
-						if (resource.getErrors().size() > 0)	return false;  
-						
-						/* - Size of the document is empty */
-						if (resource.getContents().size() == 0)  return false;	
-						
-						/* - Custom validation rule errors */
-						Diagnostic diagnostic = Diagnostician.INSTANCE.validate(resource.getContents().get(0));
-						if (diagnostic.getSeverity() == Diagnostic.ERROR)  return false; 
-						
-						/* Else */
-						return true;
-	                }
-	    });
-		
+
+		boolean result = xtextDocument.readOnly(new IUnitOfWork<Boolean, XtextResource>() {
+			public Boolean exec(XtextResource resource) throws Exception {
+				EcoreUtil.resolveAll(resource);
+
+				/* Searching for errors inside the document? */
+				/* - Error with the syntax of the dsl */
+				if (resource.getErrors().size() > 0)
+					return false;
+
+				/* - Size of the document is empty */
+				if (resource.getContents().size() == 0)
+					return false;
+
+				/* - Custom validation rule errors */
+				Diagnostic diagnostic = Diagnostician.INSTANCE.validate(resource.getContents().get(0));
+				if (diagnostic.getSeverity() == Diagnostic.ERROR)
+					return false;
+
+				/* Else */
+				return true;
+			}
+		});
+
 		return result;
 	}
 
@@ -62,19 +75,22 @@ public class Generate {
 		/* Find the editors ... */
 		XtextEditor xtextEditor = EditorUtils.getActiveXtextEditor();
 		if (xtextEditor == null) {
-			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error", "Could not locate the current Xtext editor.");
+			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error",
+					"Could not locate the current Xtext editor.");
 			return;
 		}
 
-		MultiPageEditor multipageEditor = (MultiPageEditor) ((MultiPageEditorSite) xtextEditor.getSite()).getMultiPageEditor();
+		MultiPageEditor multipageEditor = (MultiPageEditor) ((MultiPageEditorSite) xtextEditor.getSite())
+				.getMultiPageEditor();
 		if (multipageEditor == null) {
-			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error", "Could not locate the MultiPageEditor.");
+			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error",
+					"Could not locate the MultiPageEditor.");
 			return;
 		}
 
 		/* Reset the views */
-//		multipageEditor.resetView();
-		
+		// multipageEditor.resetView();
+
 		/* Retrieve the URI from the current model */
 		URI modelURI = xtextEditor.getDocument().priorityReadOnly(new IUnitOfWork<URI, XtextResource>() {
 			public URI exec(XtextResource model) throws Exception {
@@ -86,10 +102,10 @@ public class Generate {
 			MessageDialog.openError(xtextEditor.getShell(), "Error", "Could not locate the URI for the input model.");
 			return;
 		}
-		
+
 		/*
-		 * Open the dialog to choose between simple and advanced
-		 * mode for finding solutions
+		 * Open the dialog to choose between simple and advanced mode for finding
+		 * solutions
 		 */
 		SearchParametersDialog searchParamDlg = new SearchParametersDialog(xtextEditor.getShell());
 		if (searchParamDlg.open() == org.eclipse.jface.window.Window.OK) {
@@ -101,7 +117,8 @@ public class Generate {
 			findSolutionsJob.setMaxSolutions(searchParamDlg.getNumberOfSolutions());
 			findSolutionsJob.setMaxSearchTime(searchParamDlg.getSearchTime());
 			findSolutionsJob.setSavePartialSolution(searchParamDlg.getSavePartialSolution());
-			findSolutionsJob.setEnableRestarts(searchParamDlg.getEnableRestarts(), searchParamDlg.getRestartFailCount());
+			findSolutionsJob.setEnableRestarts(searchParamDlg.getEnableRestarts(),
+					searchParamDlg.getRestartFailCount());
 			findSolutionsJob.setNoGoodRecording(searchParamDlg.getNoGoodRecordingRDC());
 			findSolutionsJob.setEnableMinimization(searchParamDlg.getEnableMinimization());
 			findSolutionsJob.setPriority(Job.LONG);
