1685a130063fec0edab7290a696af3fa487edf85
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-03-16 10:44:43 +0100

    [#84]: Open wiki in the browser as help
    Added menu items
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/84

---
+++ ch.hilbri.assist.gui/src/ch/hilbri/assist/gui/handlers/HelpHandler.java
@@ -0,0 +1,12 @@
+package ch.hilbri.assist.gui.handlers;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+
+public class HelpHandler {
+	@Execute
+	public void execute(IEclipseContext context, IWorkbench workbench) {
+		// run me
+	}
+}
