7e4f0aba3fffbc08abed1b2fe741f2391db90b0e
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-03-19 08:34:59 +0100

    [#84]: Open wiki in the browser as help
    Improved the browser by adding a toolbar
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/84

--- ch.hilbri.assist.gui/src/ch/hilbri/assist/gui/parts/HelpBrowserView.java
+++ ch.hilbri.assist.gui/src/ch/hilbri/assist/gui/parts/HelpBrowserView.java
@@ -5,19 +5,23 @@ import javax.annotation.PostConstruct;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.wb.swt.ResourceManager;
 import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.browser.LocationAdapter;
+import org.eclipse.swt.browser.LocationEvent;
 
 public class HelpBrowserView {
 	private Browser browser;
+	private Text txtURL;
 
 	@PostConstruct
 	public void postConstruct(Composite parent) {
@@ -25,12 +29,12 @@ public class HelpBrowserView {
 
 		Composite composite = new Composite(parent, SWT.EMBEDDED);
 		composite.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
-		composite.setLayout(new GridLayout(1, false));
+		composite.setLayout(new GridLayout(3, false));
 
-		ToolBar toolBar = new ToolBar(composite, SWT.FLAT | SWT.RIGHT);
-		toolBar.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		ToolBar toolBarLeft = new ToolBar(composite, SWT.FLAT | SWT.RIGHT);
+		toolBarLeft.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 
-		ToolItem tltmGoBack = new ToolItem(toolBar, SWT.NONE);
+		ToolItem tltmGoBack = new ToolItem(toolBarLeft, SWT.NONE);
 		tltmGoBack.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -39,7 +43,7 @@ public class HelpBrowserView {
 		});
 		tltmGoBack.setImage(ResourceManager.getPluginImage("ch.hilbri.assist.gui", "icons/prev.gif"));
 
-		ToolItem tltmGoHome = new ToolItem(toolBar, SWT.NONE);
+		ToolItem tltmGoHome = new ToolItem(toolBarLeft, SWT.NONE);
 		tltmGoHome.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -48,7 +52,7 @@ public class HelpBrowserView {
 		});
 		tltmGoHome.setImage(ResourceManager.getPluginImage("ch.hilbri.assist.gui", "icons/home.gif"));
 
-		ToolItem tltmGoForward = new ToolItem(toolBar, SWT.NONE);
+		ToolItem tltmGoForward = new ToolItem(toolBarLeft, SWT.NONE);
 		tltmGoForward.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -56,10 +60,38 @@ public class HelpBrowserView {
 			}
 		});
 		tltmGoForward.setImage(ResourceManager.getPluginImage("ch.hilbri.assist.gui", "icons/next.gif"));
+		
+		txtURL = new Text(composite, SWT.BORDER);
+		txtURL.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				browser.setUrl(txtURL.getText());
+			}
+		});
+		txtURL.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		ToolBar toolBarRight = new ToolBar(composite, SWT.FLAT | SWT.RIGHT);
+		toolBarRight.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		
+		ToolItem tltmGo = new ToolItem(toolBarRight, SWT.NONE);
+		tltmGo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				browser.setUrl(txtURL.getText());
+			}
+		});
+		tltmGo.setImage(ResourceManager.getPluginImage("ch.hilbri.assist.gui", "icons/run.gif"));
 
 		browser = new Browser(composite, SWT.NONE);
-		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		browser.addLocationListener(new LocationAdapter() {
+			@Override
+			public void changing(LocationEvent event) {
+				txtURL.setText(event.location);
+			}
+		});
+		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1));
 		browser.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 		browser.setUrl("https://github.com/roberthilbrich/assist-public/wiki");
+
 	}
 }
