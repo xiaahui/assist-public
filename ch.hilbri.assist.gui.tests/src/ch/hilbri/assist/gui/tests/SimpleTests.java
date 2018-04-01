306e0a9019c0720302869a3061bd71afa9a5a467
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-04-01 19:40:43 +0200

    [#93] Add GUI tests with SWTBot
    Added more code
    
    Url: https://github.com/roberthilbrich/assist-public/issues/93

--- ch.hilbri.assist.gui.tests/src/ch/hilbri/assist/gui/tests/SimpleTests.java
+++ ch.hilbri.assist.gui.tests/src/ch/hilbri/assist/gui/tests/SimpleTests.java
@@ -1,5 +1,31 @@
 package ch.hilbri.assist.gui.tests;
 
-public class SimpleTests {
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class SimpleTests {
+    private static SWTBot bot;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        bot = new SWTBot();
+    }
+
+    @Test
+    public void executeExit() {
+        SWTBotMenu fileMenu = bot.menu("File");
+        Assert.assertNotNull(fileMenu);
+    }
+
+    @AfterClass
+    public static void sleep() {
+        bot.sleep(2000);
+    }
 }
