444514908d9f9fa28e30a93fe6f6c8ca93d4da36
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-05-19 15:09:12 +0200

    [#61] Import from Amalthea model
    Added import menu item and templates for mapping / scheduling importer
    from APP4MC models
    
    Url: https://github.com/roberthilbrich/assist-public/issues/61

---
+++ ch.hilbri.assist.scheduling/src/ch/hilbri/assist/scheduling/ui/wizards/ImportAPP4MCWizard.java
@@ -0,0 +1,44 @@
+package ch.hilbri.assist.scheduling.ui.wizards;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ImportAPP4MCWizard extends Wizard implements IImportWizard {
+	
+	ImportAPP4MCWizardPage mainPage;
+
+	public ImportAPP4MCWizard() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	public boolean performFinish() {
+		IFile file = mainPage.createNewFile();
+        if (file == null)
+            return false;
+        return true;
+	}
+	 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("File Import Wizard"); //NON-NLS-1
+		setNeedsProgressMonitor(true);
+		mainPage = new ImportAPP4MCWizardPage("Import APP4MC to Scheduling",selection); //NON-NLS-1
+	}
+	
+	/* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.IWizard#addPages()
+     */
+    public void addPages() {
+        super.addPages(); 
+        addPage(mainPage);        
+    }
+
+}
