cb7cba70524f7448b1e6aa163f7ca05aa0b1c830
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-06-07 14:23:35 +0200

    [#109]: Redesign of mapping results view
    Added the chart properly - incl. font sizes
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/109

--- ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/multipageeditor/DetailedResultsPage.java
+++ ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/multipageeditor/DetailedResultsPage.java
@@ -6,6 +6,7 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -38,7 +39,7 @@ public class DetailedResultsPage extends Composite {
         formToolkit.decorateFormHeading(mainForm.getForm());
         FontDescriptor fontDescriptor = FontDescriptor.createFrom(mainForm.getFont()).setStyle(SWT.BOLD).increaseHeight(1);
         mainForm.setFont(fontDescriptor.createFont(mainForm.getDisplay()));
-        mainForm.setText("Results");
+        mainForm.setText("Mapping Results");
         IToolBarManager toolbarManager = mainForm.getToolBarManager();  
         fillToolBar(toolbarManager);
         mainForm.getBody().setLayout(new GridLayout(1, false));
@@ -51,10 +52,15 @@ public class DetailedResultsPage extends Composite {
         Composite compAllSolutions = formToolkit.createComposite(sctnAllSolutions, SWT.NONE);
         formToolkit.paintBordersFor(compAllSolutions);
         sctnAllSolutions.setClient(compAllSolutions);
-        compAllSolutions.setLayout(new GridLayout(1, false));
+        GridLayout gl_compAllSolutions = new GridLayout(1, false);
+        gl_compAllSolutions.marginTop = 5;
+        gl_compAllSolutions.marginHeight = 0;
+        gl_compAllSolutions.horizontalSpacing = 0;
+        gl_compAllSolutions.verticalSpacing = 0;
+        gl_compAllSolutions.marginWidth = 0;
+        compAllSolutions.setLayout(gl_compAllSolutions);
         
         createScoreChart(compAllSolutions);
-        
         
         /* ************** SECTION: Current Solution ******************** */
         Section sctnCurrentSolution = formToolkit.createSection(mainForm.getBody(), Section.TITLE_BAR);
@@ -62,21 +68,27 @@ public class DetailedResultsPage extends Composite {
         formToolkit.paintBordersFor(sctnCurrentSolution);
         sctnCurrentSolution.setText("Current Solution");   
         Composite compCurrentSolution = formToolkit.createComposite(sctnCurrentSolution, SWT.NONE);
+        compCurrentSolution.setBackground(SWTResourceManager.getColor(SWT.COLOR_INFO_BACKGROUND));
         formToolkit.paintBordersFor(compCurrentSolution);
         sctnCurrentSolution.setClient(compCurrentSolution);
         
         /* ************** SECTION: Evaluation ******************** */
-        Section sctnEvaluation = formToolkit.createSection(mainForm.getBody(), Section.EXPANDED | Section.TITLE_BAR);
+        Section sctnEvaluation = formToolkit.createSection(mainForm.getBody(), Section.TWISTIE | Section.TITLE_BAR);
         sctnEvaluation.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
         formToolkit.paintBordersFor(sctnEvaluation);
         sctnEvaluation.setText("Evaluation");
         Composite compEvaluation = formToolkit.createComposite(sctnEvaluation, SWT.NONE);
+        compEvaluation.setBackground(SWTResourceManager.getColor(SWT.COLOR_INFO_BACKGROUND));
         formToolkit.paintBordersFor(compEvaluation);
         sctnEvaluation.setClient(compEvaluation);
         
     }
     
-    
+    /**
+     * Creates the score chart
+     * 
+     * @param composite parent composite
+     */
     private void createScoreChart(Composite composite) {
         scoreOverview = new Chart(composite, SWT.NONE);
         GridData gd_scoreOverview = new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1);
@@ -84,26 +96,35 @@ public class DetailedResultsPage extends Composite {
         scoreOverview.setLayoutData(gd_scoreOverview);
         scoreOverview.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
         scoreOverview.getTitle().setVisible(false);
+        
         IAxis xaxes = scoreOverview.getAxisSet().getXAxes()[0];
         IAxis yaxes = scoreOverview.getAxisSet().getYAxes()[0];
+        
         scoreOverview.getAxisSet().adjustRange();
+        xaxes.setRange(new Range(0, 10));
+        
         xaxes.getTick().setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GRAY));
-        xaxes.getTick().setFont(SWTResourceManager.getFont("Segoe UI", 7, SWT.NORMAL));
         xaxes.getTitle().setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
-        xaxes.getTitle().setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
-        xaxes.getTitle().setText("Solutions");
         xaxes.getGrid().setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
         xaxes.getGrid().setStyle(LineStyle.NONE);
-        xaxes.setRange(new Range(0, 10));
+        xaxes.getTitle().setText("Solutions");
         xaxes.getTick().setFormat(new DecimalFormat("#"));
+        
         yaxes.getTick().setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GRAY));
-        yaxes.getTick().setFont(SWTResourceManager.getFont("Segoe UI", 7, SWT.NORMAL));
         yaxes.getTitle().setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
-        yaxes.getTitle().setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
-        yaxes.getTitle().setText("Total score (scaled)");
         yaxes.getGrid().setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
         yaxes.getGrid().setStyle(LineStyle.DOT);
+        yaxes.getTitle().setText("Score");
         yaxes.getTick().setTickMarkStepHint(30);
+
+        Display display = composite.getDisplay();
+        Font smallFont = FontDescriptor.createFrom(xaxes.getTick().getFont()).setStyle(SWT.BOLD).increaseHeight(-1).createFont(display);
+        Font xsmallFont = FontDescriptor.createFrom(xaxes.getTick().getFont()).increaseHeight(-2).createFont(display);
+        
+        xaxes.getTitle().setFont(smallFont);
+        yaxes.getTitle().setFont(smallFont);
+        xaxes.getTick().setFont(xsmallFont);
+        yaxes.getTick().setFont(smallFont);
     }
     
     /**
