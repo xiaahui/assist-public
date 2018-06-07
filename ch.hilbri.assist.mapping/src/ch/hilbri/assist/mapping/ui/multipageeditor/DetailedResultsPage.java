bc4eea0c86e9426dcccb9ca4b5849e2890d261d5
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-06-07 14:08:25 +0200

    [#109]: Redesign of mapping results view
    Added the SWT chart
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/109

--- ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/multipageeditor/DetailedResultsPage.java
+++ ch.hilbri.assist.mapping/src/ch/hilbri/assist/mapping/ui/multipageeditor/DetailedResultsPage.java
@@ -1,5 +1,7 @@
 package ch.hilbri.assist.mapping.ui.multipageeditor;
 
+import java.text.DecimalFormat;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.resource.FontDescriptor;
@@ -13,10 +15,16 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.swtchart.Chart;
+import org.swtchart.IAxis;
+import org.swtchart.LineStyle;
+import org.swtchart.Range;
 
 public class DetailedResultsPage extends Composite {
     private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-
+    private Chart scoreOverview;
+    
     /**
      * Create the composite.
      * @param parent
@@ -28,45 +36,75 @@ public class DetailedResultsPage extends Composite {
         
         ScrolledForm mainForm = formToolkit.createScrolledForm(this);
         formToolkit.decorateFormHeading(mainForm.getForm());
-        
         FontDescriptor fontDescriptor = FontDescriptor.createFrom(mainForm.getFont()).setStyle(SWT.BOLD).increaseHeight(1);
         mainForm.setFont(fontDescriptor.createFont(mainForm.getDisplay()));
-        
         mainForm.setText("Results");
-        
         IToolBarManager toolbarManager = mainForm.getToolBarManager();  
         fillToolBar(toolbarManager);
-        
         mainForm.getBody().setLayout(new GridLayout(1, false));
-        
+
+        /* ************** SECTION: All Solutions ******************** */
         Section sctnAllSolutions = formToolkit.createSection(mainForm.getBody(), Section.EXPANDED | Section.TITLE_BAR);
         sctnAllSolutions.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
         formToolkit.paintBordersFor(sctnAllSolutions);
         sctnAllSolutions.setText("All Solutions");
-        
         Composite compAllSolutions = formToolkit.createComposite(sctnAllSolutions, SWT.NONE);
         formToolkit.paintBordersFor(compAllSolutions);
         sctnAllSolutions.setClient(compAllSolutions);
+        compAllSolutions.setLayout(new GridLayout(1, false));
         
+        createScoreChart(compAllSolutions);
+        
+        
+        /* ************** SECTION: Current Solution ******************** */
         Section sctnCurrentSolution = formToolkit.createSection(mainForm.getBody(), Section.TITLE_BAR);
         sctnCurrentSolution.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
         formToolkit.paintBordersFor(sctnCurrentSolution);
         sctnCurrentSolution.setText("Current Solution");   
-        
         Composite compCurrentSolution = formToolkit.createComposite(sctnCurrentSolution, SWT.NONE);
         formToolkit.paintBordersFor(compCurrentSolution);
         sctnCurrentSolution.setClient(compCurrentSolution);
         
+        /* ************** SECTION: Evaluation ******************** */
         Section sctnEvaluation = formToolkit.createSection(mainForm.getBody(), Section.EXPANDED | Section.TITLE_BAR);
         sctnEvaluation.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
         formToolkit.paintBordersFor(sctnEvaluation);
         sctnEvaluation.setText("Evaluation");
-        
         Composite compEvaluation = formToolkit.createComposite(sctnEvaluation, SWT.NONE);
         formToolkit.paintBordersFor(compEvaluation);
         sctnEvaluation.setClient(compEvaluation);
+        
     }
     
+    
+    private void createScoreChart(Composite composite) {
+        scoreOverview = new Chart(composite, SWT.NONE);
+        GridData gd_scoreOverview = new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1);
+        gd_scoreOverview.heightHint = 125;
+        scoreOverview.setLayoutData(gd_scoreOverview);
+        scoreOverview.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
+        scoreOverview.getTitle().setVisible(false);
+        IAxis xaxes = scoreOverview.getAxisSet().getXAxes()[0];
+        IAxis yaxes = scoreOverview.getAxisSet().getYAxes()[0];
+        scoreOverview.getAxisSet().adjustRange();
+        xaxes.getTick().setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GRAY));
+        xaxes.getTick().setFont(SWTResourceManager.getFont("Segoe UI", 7, SWT.NORMAL));
+        xaxes.getTitle().setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+        xaxes.getTitle().setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
+        xaxes.getTitle().setText("Solutions");
+        xaxes.getGrid().setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+        xaxes.getGrid().setStyle(LineStyle.NONE);
+        xaxes.setRange(new Range(0, 10));
+        xaxes.getTick().setFormat(new DecimalFormat("#"));
+        yaxes.getTick().setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GRAY));
+        yaxes.getTick().setFont(SWTResourceManager.getFont("Segoe UI", 7, SWT.NORMAL));
+        yaxes.getTitle().setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+        yaxes.getTitle().setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
+        yaxes.getTitle().setText("Total score (scaled)");
+        yaxes.getGrid().setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+        yaxes.getGrid().setStyle(LineStyle.DOT);
+        yaxes.getTick().setTickMarkStepHint(30);
+    }
     
     /**
      * Fill the items into the toolbar
