bec9e53ad26ae806084417a8c71e5d168ceb9b5a
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-06-27 11:26:40 +0200

    [#82]: Generate Mapping Problem Specifications out of DSE
    Some more UI elements
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/82

--- ch.hilbri.assist.dse/src/ch/hilbri/assist/dse/ui/multipageeditor/DetailedResults.java
+++ ch.hilbri.assist.dse/src/ch/hilbri/assist/dse/ui/multipageeditor/DetailedResults.java
@@ -1,8 +1,20 @@
 package ch.hilbri.assist.dse.ui.multipageeditor;
 
+import org.eclipse.jface.resource.FontDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
 
 public class DetailedResults extends Composite {
+    private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+    private Table table;
 
     /**
      * Create the composite.
@@ -11,7 +23,32 @@ public class DetailedResults extends Composite {
      */
     public DetailedResults(Composite parent, int style) {
         super(parent, style);
-
+        setLayout(new FillLayout(SWT.HORIZONTAL));
+        
+        ScrolledForm mainForm = formToolkit.createScrolledForm(this);
+        formToolkit.decorateFormHeading(mainForm.getForm());
+        FontDescriptor fontDescriptor = FontDescriptor.createFrom(mainForm.getFont());
+        fontDescriptor.setStyle(SWT.BOLD).increaseHeight(1);
+        mainForm.setFont(fontDescriptor.createFont(mainForm.getDisplay()));
+        mainForm.setText("Exploration Results");
+        
+        mainForm.getBody().setLayout(new GridLayout(1, false));
+        
+        Section sctnOverview = formToolkit.createSection(mainForm.getBody(), Section.EXPANDED | Section.TITLE_BAR);
+        sctnOverview.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+        formToolkit.paintBordersFor(sctnOverview);
+        sctnOverview.setText("Overview");
+        Composite compOverview = formToolkit.createComposite(sctnOverview, SWT.NONE);
+        formToolkit.paintBordersFor(compOverview);
+        sctnOverview.setClient(compOverview);
+        compOverview.setLayout(new GridLayout(1, false));
+        
+        table = new Table(compOverview, SWT.BORDER | SWT.FULL_SELECTION);
+        table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+        formToolkit.adapt(table);
+        formToolkit.paintBordersFor(table);
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
     }
 
     @Override
