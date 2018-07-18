690d3a47d625f336d9505a97e4b1c8477a18d431
Robert Hilbrich <robert.hilbrich@dlr.de>
2018-07-18 08:31:17 +0200

    [#115]: New Spec Wizard acts weird with preselection
    Adopted Changes to DSE Spec Wizard
    
    Task-Url: http://github.com/roberthilbrich/assist-public/issues/115

--- ch.hilbri.assist.dse/src/ch/hilbri/assist/dse/ui/wizards/NewDSESpecificationWizardPage.java
+++ ch.hilbri.assist.dse/src/ch/hilbri/assist/dse/ui/wizards/NewDSESpecificationWizardPage.java
@@ -4,6 +4,8 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.internal.core.JavaElement;
+import org.eclipse.jdt.internal.ui.packageview.PackageFragmentRootContainer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardPage;
@@ -22,165 +24,189 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ContainerSelectionDialog;
 
+@SuppressWarnings("restriction")
 public class NewDSESpecificationWizardPage extends WizardPage {
 
-	private Text containerText;
-	private Text fileText;
-	private ISelection selection;
+    private Text containerText;
+    private Text fileText;
+    private ISelection selection;
 
-	public NewDSESpecificationWizardPage(ISelection selection) {
-		super("Create Specification");
-		setTitle("Create new DSE specification");
-		setDescription("This wizard creates a new design space exploration specification file with *.ddsl extension.");
-		this.selection = selection;
-	}
+    public NewDSESpecificationWizardPage(ISelection selection) {
+        super("Create Specification");
+        setTitle("Create new DSE specification");
+        setDescription("This wizard creates a new design space exploration specification file with *.ddsl extension.");
+        this.selection = selection;
+    }
 
-	@Override
-	public void createControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NULL);
-		GridLayout layout = new GridLayout();
-		container.setLayout(layout);
-		layout.numColumns = 3;
-		layout.verticalSpacing = 9;
+    @Override
+    public void createControl(Composite parent) {
+        Composite container = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        container.setLayout(layout);
+        layout.numColumns = 3;
+        layout.verticalSpacing = 9;
 
-		Label fileNameLabel = new Label(container, SWT.NULL);
-		fileNameLabel.setText("&File name:");
+        Label fileNameLabel = new Label(container, SWT.NULL);
+        fileNameLabel.setText("&File name:");
 
-		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
 
-		fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		fileText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				dialogChanged();
-			}
-		});
+        fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        fileText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
 
-		Label fileNameExtensionLabel = new Label(container, SWT.NULL);
-		fileNameExtensionLabel.setText(".ddsl");
+        Label fileNameExtensionLabel = new Label(container, SWT.NULL);
+        fileNameExtensionLabel.setText(".ddsl");
 
-		Label projectLabel = new Label(container, SWT.NULL);
-		projectLabel.setText("&Project:");
+        Label projectLabel = new Label(container, SWT.NULL);
+        projectLabel.setText("&Project:");
 
-		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		containerText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		containerText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				dialogChanged();
-			}
-		});
+        containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        containerText.setEditable(false);
+        containerText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        containerText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
 
-		Button button = new Button(container, SWT.PUSH);
-		button.setText("Browse...");
-		button.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				handleBrowse();
-			}
-		});
+        Button button = new Button(container, SWT.PUSH);
+        button.setText("Browse...");
+        button.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                handleBrowse();
+            }
+        });
 
-		initialize();
-		dialogChanged();
-		setControl(container);
-	}
+        initialize();
+        dialogChanged();
+        setControl(container);
+    }
 
-	/**
-	 * Tests if the current workbench selection is a suitable container to use.
-	 */
+    /**
+     * Tests if the current workbench selection is a suitable container to use.
+     */
+    private void initialize() {
+        if (selection.isEmpty()) {
+            IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+            selection = window.getSelectionService().getSelection("org.eclipse.ui.navigator.ProjectExplorer");
+        }
 
-	private void initialize() {
-		if (selection == null) {
-			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-			selection = window.getSelectionService().getSelection("org.eclipse.ui.navigator.ProjectExplorer");
-		}
+        if (selection != null && selection.isEmpty() == false && selection instanceof IStructuredSelection) {
+            IStructuredSelection ssel = (IStructuredSelection) selection;
+            if (ssel.size() > 1)
+                return;
+            Object obj = ssel.getFirstElement();
+            IContainer container;
 
-		if (selection != null && selection.isEmpty() == false && selection instanceof IStructuredSelection) {
-			IStructuredSelection ssel = (IStructuredSelection) selection;
-			if (ssel.size() > 1)
-				return;
-			Object obj = ssel.getFirstElement();
-			if (obj instanceof IResource) {
-				IContainer container;
-				if (obj instanceof IContainer)
-					container = (IContainer) obj;
-				else
-					container = ((IResource) obj).getParent();
+            // If a project or a file is preselected 
+            if (obj instanceof IResource) {
+                if (obj instanceof IContainer)
+                    container = (IContainer) obj;
+                else
+                    container = ((IResource) obj).getProject();
 
-				containerText.setText(container.getFullPath().toString());
-			}
-		}
+                // No leading "/"
+                containerText.setText(container.getFullPath().toString().substring(1));
+                
+            } 
+            
+            // If a package (subfolder) is preselected)
+            else if (obj instanceof JavaElement) {
+                container = ((JavaElement) obj).getResource().getProject();
 
-		fileText.setText("newSpecification");
-	}
+                // No leading "/"
+                containerText.setText(container.getFullPath().toString().substring(1));
+            }
+            
+            // If the classpath container got selected
+            else if (obj instanceof PackageFragmentRootContainer) {
+                container = ((PackageFragmentRootContainer) obj).getJavaProject().getProject();
 
-	/**
-	 * Uses the standard container selection dialog to choose the new value for the
-	 * container field.
-	 */
+                // No leading "/"
+                containerText.setText(container.getFullPath().toString().substring(1));
+            }
 
-	private void handleBrowse() {
-		ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(),
-				ResourcesPlugin.getWorkspace().getRoot(), false, "Select project");
-		if (dialog.open() == ContainerSelectionDialog.OK) {
-			Object[] result = dialog.getResult();
-			if (result.length == 1) {
-				Path path = (Path) result[0];
-				if (path.segmentCount() > 1)
-					path = (Path) path.removeLastSegments(path.segmentCount() - 1);
-				containerText.setText(path.toString());
-			}
-		}
-	}
+        }
 
-	/**
-	 * Ensures that both text fields are set.
-	 */
+        fileText.setText("newSpecification");
+    }
 
-	private void dialogChanged() {
-		IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(getContainerName()));
-		String fileName = getFileName();
+    /**
+     * Uses the standard container selection dialog to choose the new value for the
+     * container field.
+     */
 
-		if (getContainerName().length() == 0) {
-			updateStatus("Project must be specified");
-			return;
-		}
-		if (container == null || (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
-			updateStatus("Project must exist");
-			return;
-		}
-		if (!container.isAccessible()) {
-			updateStatus("Project must be writable");
-			return;
-		}
-		if (fileName.length() == 0) {
-			updateStatus("File name must be specified");
-			return;
-		}
-		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
-			updateStatus("File name must be valid");
-			return;
-		}
-		IResource temp = ResourcesPlugin.getWorkspace().getRoot()
-				.findMember(new Path(getContainerName() + "/Exploration/" + fileName + ".ddsl"));
-		if (temp != null) {
-			if (temp.exists()) {
-				updateStatus("File already exists");
-				return;
-			}
-		}
+    private void handleBrowse() {
+        ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(),
+                ResourcesPlugin.getWorkspace().getRoot(), false, "Select project");
+        if (dialog.open() == ContainerSelectionDialog.OK) {
+            Object[] result = dialog.getResult();
+            if (result.length == 1) {
+                Path path = (Path) result[0];
+                if (path.segmentCount() > 1)
+                    path = (Path) path.removeLastSegments(path.segmentCount() - 1);
 
-		updateStatus(null);
-	}
+                // No leading "/"
+                containerText.setText(path.toString().substring(1));
+            }
+        }
+    }
 
-	private void updateStatus(String message) {
-		setErrorMessage(message);
-		setPageComplete(message == null);
-	}
+    /**
+     * Ensures that both text fields are set.
+     */
 
-	public String getContainerName() {
-		return containerText.getText();
-	}
+    private void dialogChanged() {
+        IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(getContainerName()));
+        String fileName = getFileName();
 
-	public String getFileName() {
-		return fileText.getText();
-	}
+        if (getContainerName().length() == 0) {
+            updateStatus("Project must be specified");
+            return;
+        }
+        if (container == null || (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+            updateStatus("Project must exist");
+            return;
+        }
+        if (!container.isAccessible()) {
+            updateStatus("Project must be writable");
+            return;
+        }
+        if (fileName.length() == 0) {
+            updateStatus("File name must be specified");
+            return;
+        }
+        if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+            updateStatus("File name must be valid");
+            return;
+        }
+        IResource temp = ResourcesPlugin.getWorkspace().getRoot()
+                .findMember(new Path(getContainerName() + "/Exploration/" + fileName + ".ddsl"));
+        if (temp != null) {
+            if (temp.exists()) {
+                updateStatus("File already exists");
+                return;
+            }
+        }
+
+        updateStatus(null);
+    }
+
+    private void updateStatus(String message) {
+        setErrorMessage(message);
+        setPageComplete(message == null);
+    }
+
+    public String getContainerName() {
+        return containerText.getText();
+    }
+
+    public String getFileName() {
+        return fileText.getText();
+    }
 
 }
